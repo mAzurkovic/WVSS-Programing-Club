08de26d83c874cbd053200698fe9b4629ba1e8b2
mAzurkovic <mattias@zurkovic.com>
2015-10-27 20:36:12 -0700

    add solution

---
+++ pset1_solution.py
@@ -0,0 +1,12 @@
+'''
+The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+'''
+
+weekday = True
+vacation = True
+
+if not weekday or vacation:
+    print(True)
+else:
+    print(False)
